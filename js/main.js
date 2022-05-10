aee21c59316a2e6534a5963565f4723ad1ae1a5f
An Tsv <example@gmail.com>
2022-05-10 17:24:01 +0300

    table

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
 const equals = document.getElementById('equals');
 const firstNumInput = document.getElementById('num-1');
 const secondNumInput = document.getElementById('num-2');
+const historyTable = document.querySelector('.history__calc');
+const historyOperations = [];
+let i = 0;
 
 equals.addEventListener('click', () => {
 	const a = firstNumInput.value;
 	const b = secondNumInput.value;
 	const selectOperation = document.getElementById('select').value;
 	const lastResult = document.querySelector('.oparation_result span');
-	
+	const result = calculate(a, b, selectOperation);
+
 	if(a && b) {
-		lastResult.textContent = calculate(a, b, selectOperation);
+		lastResult.textContent = result;
 	} else {
 		lastResult.textContent = 'error';
 	}
 
 	clearInputs();
+
+	createHistoryList({
+		a,
+		b,
+		operation: selectOperation,
+		result
+	});
 });
+
 
 const clearInputs = () => {
 	firstNumInput.value = '';
@@ -28,22 +40,50 @@ const calculate = (num, secondNum, operation) => {
 	let result = 0;
 
 	switch(operation) {
-		case 'plus':
+		case '+':
 			result = a + b;
 			break;
-		case 'minus':
+		case '-':
 			result = a - b;
 			break;
-		case 'multiply':
+		case '*':
 			result = a * b;
 			break;
-		case 'divide':
+		case '/':
 			result = a / b;
 			break;
 		case 'sqrt':
-			result = Math.SQRT${b}(a);
+			result = Math.sqrt(a);
 			break;
 	}
 
 	return result;
 };
+
+
+function createHistoryList(operation) {
+	historyOperations.unshift(operation);
+
+	historyTable.innerHTML = "";
+	historyTable.innerHTML += `
+		<tr class="history__calc_title">
+			<th>№</th>
+			<th>Number 1</th>
+			<th>Operation</th>
+			<th>Number 2</th>
+			<th>Equals</th>
+		</tr>
+	`;
+
+	historyOperations.forEach((historyOperation, i) => {
+		historyTable.innerHTML += `
+			<tr class="history__interactive-list">
+				<td>${i + 1}</td>
+				<td>${historyOperation.a}</td>
+				<td>${historyOperation.operation}</td>
+				<td>${historyOperation.b}</td>
+				<td>${historyOperation.result}</td>
+			</tr>
+			`;
+	});
+}
